d5e330fb1929074f29e0f9f7c0027b9386e0a5d0
chence <chence@shinevv.com>
2018-03-05 17:54:30 +0800

    增加文档，增加demo

---
+++ shinevv_demo/include/shinevv.h
@@ -0,0 +1,212 @@
+﻿#ifndef SHINEVV_H
+#define SHINEVV_H
+
+#ifdef SHINEVV_INTERFACE_EXPORTS  
+#define SHINEVV_INTERFACE_API __declspec(dllexport)   
+#else  
+#define SHINEVV_INTERFACE_API __declspec(dllimport)   
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+	/**
+	* 错误码
+	* @VVSuccess						加入房间成功
+	* @VVConnectFail					连接服务器失败
+	* @VVCreateMediaSessionFail			创建媒体会话失败
+	* @VVAlreadyJoined					重复加入
+	*/
+	enum ErrorCode {
+		VVSuccess = 0,
+		VVConnectFail,
+		VVCreateMediaSessionFail,
+		VVAlreadyJoined
+	};
+
+	/**
+	* 媒体会话类型
+	* @Audio					语音通话
+	* @Video					视频通话
+	*/
+	enum MediaType {
+		Audio = 0,
+		Video
+	};
+
+	/**
+	* 创建shinevv句柄
+	* 业务逻辑 跟随主程序一起创建
+	* @return shinevv句柄
+	*/
+	SHINEVV_INTERFACE_API void* __stdcall CreateShinevvInstance();
+
+	/**
+	* 销毁Shinevv句柄
+	* 业务逻辑 在主程序要退出时调用
+	* @param handle				Shinevv句柄
+	*/
+	SHINEVV_INTERFACE_API void __stdcall DestoryShinevvInstance();
+
+	/**
+	* 回收内部资源，在程序结束时调用
+	* 业务逻辑 在主程序要退出时调用(先调用DestoryShinevvInstance()，再调用ClearShinevv() )
+	*/
+	SHINEVV_INTERFACE_API void __stdcall ClearShinevv();
+
+	/**
+	* 初始化shinevvSDK.
+	* 业务逻辑 在创建房间时调用
+	* @param handle							shinevv句柄
+	* @param pRoomId						房间号
+	* @param eMediaType						媒体会话类型
+	* @param pMemberId						成员标识符，必须在当前房间中保持唯一
+	* @param pDisplayName					成员昵称
+	* @param pServerIp						服务器IP
+	* @param nPort							服务器端口
+	* @param pToken							认证信息,此为预留接口，目前还没有认证，这里可以给一个任意字符串
+	* @param OnJoined						加入房间回调,调用JoinRoom后会触发
+	* @param OnDisConnected					断开连接回调,当此回调被触发时，要依次调用LeaveRoom()， ReleaseSdk()来释放资源
+	* @param OnNewMemberJoined				新成员加入房间回调，在此回调中调用SetRenderWindow()接口来设置新成员视频渲染窗口句柄
+	* @param OnMemberLeft					成员离开房间回调，其它成员调用LeaveRoom后会触发
+	* @param OnModifyLocalAudioStatus		本地音频状态改变回调，JoinRoom(),ModifyAudioStatus(),都会触发
+	* @param OnModifyLocalVideoStatus		本地视频状态改变回调，JoinRoom(),ModifyVideoStatus(),都会触发
+	* @param OnMemberEnableVideo			成员打开视频回调
+	* @param OnMemberDisableVideo			成员关闭视频回调
+	* @param OnMemberUnMuteAudio			成员打开音频回调
+	* @param OnMemberMuteAudio				成员关闭音频回调
+	* @param OnSessionError					会话失败回调，会话失败的原因包括设备被占用，网络堵塞导致消息超时，消息内容错误等等
+	*/
+	SHINEVV_INTERFACE_API void __stdcall InitSDK(
+		void* handle,
+		const char* pRoomId,
+		MediaType eMediaType,
+		const char* pMemberId,
+		const char* pDisplayName,
+		const char* pServerIp,
+		int nPort,
+		const char* pToken,
+		void(*OnJoined)(ErrorCode eCode),
+		void(*OnDisConnected)(),
+		void(*OnNewMemberJoined)(const char* pMemberId, const char* pDisplayName),
+		void(*OnMemberLeft)(const char* pMemberId, const char* pDisplayName),
+		void(*OnModifyLocalAudioStatus)(bool bOpen),
+		void(*OnModifyLocalVideoStatus)(bool bOpen),
+		void(*OnMemberEnableVideo)(const char* pMemberId, const char* pDisplayName),
+		void(*OnMemberDisableVideo)(const char* pMemberId, const char* pDisplayName, void* pRenderWin),
+		void(*OnMemberUnMuteAudio)(const char* pMemberId, const char* pDisplayName),
+		void(*OnMemberMuteAudio)(const char* pMemberId, const char* pDisplayName),
+		void(*OnSessionError)(const char* reason));
+
+	/**
+	* 销毁shinevvSDK内部资源.
+	* 业务逻辑 在销毁房间时调用
+	* @param handle					shinevv句柄
+	*/
+	SHINEVV_INTERFACE_API void __stdcall ReleaseSdk(
+		void* handle);
+
+	/**
+	* 指定视频渲染窗口.
+	* 业务逻辑 针对本地视频，在JoinRoom()之前调用，针对远端视频在收到OnMemberEnableVideo()回调时调用
+	* @param handle					shinevv句柄
+	* @param pMemberId				成员标识符
+	* @param pRemoteRenderWin       渲染窗口句柄
+	* @param bMirror				是否镜像，一般本地图像为true，远端为false
+	*/
+	SHINEVV_INTERFACE_API void __stdcall SetRenderWindow(
+		void* handle,
+		const char* pMemberId,
+		void* pRenderWin,
+		bool bMirror);
+
+	/**
+	* 加入房间.
+	* 业务逻辑 在创建房间后，主动开启视频会议时调用
+	* @param handle					shinevv句柄
+	*/
+	SHINEVV_INTERFACE_API void __stdcall JoinRoom(
+		void* handle);
+
+	/**
+	* 离开房间.
+	* 业务逻辑 在创建房间后，主动关闭视频会议时调用
+	* @param handle					shinevv句柄
+	*/
+	SHINEVV_INTERFACE_API void __stdcall LeaveRoom(
+		void* handle);
+
+	/**
+	* 改变视频状态.
+	* 业务逻辑 一定要等JoinRoom()的回调通知后才可以调用，改变视频状态的结果由OnModifyLocalVideoStatus回调通知
+	* @param handle					shinevv句柄
+	* @param bOpen					打开或关闭视频
+	*/
+	SHINEVV_INTERFACE_API void __stdcall ModifyVideoStatus(
+		void* handle,
+		bool bOpen);
+
+	/**
+	* 改变音频状态.
+	* 业务逻辑 一定要等JoinRoom()的回调通知后才可以调用，改变视频状态的结果由OnModifyLocalAudioStatus回调通知
+	* @param handle					shinevv句柄
+	* @param bOpen					打开或关闭音频
+	*/
+	SHINEVV_INTERFACE_API void __stdcall ModifyAudioStatus(
+		void* handle,
+		bool bOpen);
+
+	/**
+	* 接收指定成员视频.
+	* 业务逻辑 默认状态下是接受所有房间成员视频，如果调用过DisableMemberVideo()后，可以通过此接口接收指定成员视频
+	* @param handle						shinevv句柄
+	* @param pMemberId					成员标识符
+	* @param EnableMemberVideoResult	接收指定成员视频回调
+	*/
+	SHINEVV_INTERFACE_API void __stdcall EnableMemberVideo(
+		void* handle,
+		const char* pMemberId,
+		void(*EnableMemberVideoResult)(bool bSucc, const char* pMemberId));
+
+	/**
+	* 不接收指定成员视频.
+	* 业务逻辑 默认状态下是接受所有房间成员视频，通过此接口可以不接收指定成员视频
+	* @param handle						shinevv句柄
+	* @param pMemberId					成员标识符
+	* @param DisableMemberVideoResult	不接收指定成员视频回调
+	*/
+	SHINEVV_INTERFACE_API void __stdcall DisableMemberVideo(
+		void* handle,
+		const char* pMemberId,
+		void(*DisableMemberVideoResult)(bool bSucc, const char* pMemberId));
+
+	/**
+	* 接收指定成员音频.
+	* 业务逻辑 默认状态下是接受所有房间成员音频，如果调用过DisableMemberAudio()后，可以通过此接口接收指定成员音频
+	* @param handle						shinevv句柄
+	* @param pMemberId					成员标识符
+	* @param EnableMemberVideoResult	接收指定成员音频回调
+	*/
+	SHINEVV_INTERFACE_API void __stdcall EnableMemberAudio(
+		void* handle,
+		const char* pMemberId,
+		void(*EnableMemberAudioResult)(bool bSucc, const char* pMemberId));
+
+	/**
+	* 不接收指定成员音频.
+	* 业务逻辑 默认状态下是接受所有房间成员音频，通过此接口可以不接收指定成员音频
+	* @param handle						shinevv句柄
+	* @param pMemberId					成员标识符
+	* @param DisableMemberAudioResult	不接收指定成员视频回调
+	*/
+	SHINEVV_INTERFACE_API void __stdcall DisableMemberAudio(
+		void* handle,
+		const char* pMemberId,
+		void(*DisableMemberAudioResult)(bool bSucc, const char* pMemberId));
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ shinevv_demo/shinevv_demo/main.cpp
@@ -0,0 +1,298 @@
+#include "../include/main.h"
+
+HWND _mainWindow;
+
+MemberWindowMap _member_win_map;
+
+void* _instance = NULL;
+
+std::string _roomId;
+
+std::string _local_peer_id;
+
+std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> _convert;
+
+LRESULT CALLBACK VVCreateWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		break;
+	case WM_CHAR:
+		switch (wParam)
+		{
+
+		case 'q':
+		{
+			_instance = CreateShinevvInstance();
+
+			if (_instance)
+				printf("CreateShinevvInstance %d\n", (int)_instance);
+
+			break;
+		}
+		case 'w':
+		{
+			DestoryShinevvInstance();
+
+			_instance = NULL;
+
+			printf("DestoryShinevvInstance OK\n");
+
+			break;
+		}
+		case 'e':
+		{
+			printf("InitSDK\n");
+
+			std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
+			std::string u8str = convert.to_bytes(DISPLAY_NAME);
+
+			_local_peer_id = MEMBER_ID;
+
+			InitSDK(_instance, _roomId.c_str(), MediaType::Video, _local_peer_id.c_str(), u8str.c_str(), "vvroom.shinevv.cn", 3443, "312", OnJoined, OnDisConnected,
+				OnNewMemberJoined, OnMemberLeft, OnModifyLocalAudioStatus, OnModifyLocalVideoStatus, OnMemberEnableVideo, OnMemberDisableVideo, OnMemberUnMuteAudio, OnMemberMuteAudio, OnSessionError);
+
+			break;
+		}
+		case 'r':
+		{
+			printf("ReleaseSdk\n");
+
+			ReleaseSdk(_instance);
+
+			break;
+		}
+		case 't':
+		{
+			printf("JoinRoom\n");
+
+			SetRenderWindow(_instance, _local_peer_id.c_str(), (void*)_mainWindow, true);
+
+			JoinRoom(_instance);
+
+			break;
+		}
+		case 'y':
+		{
+			printf("LeaveRoom\n");
+
+			LeaveRoom(_instance);
+
+			MemberWindowMap::iterator it = _member_win_map.begin();
+
+			for (it; it != _member_win_map.end(); it++)
+			{
+				auto win = it->second;
+
+				CloseWindow(win);
+			}
+
+			_member_win_map.clear();
+
+			break;
+		}
+		case 'l':// clear shinevv
+		{
+			ClearShinevv();
+
+			break;
+		}
+		case 'a':// disable camera
+		{
+			if (_instance) {
+				ModifyVideoStatus(_instance, false);
+			}
+
+			break;
+		}
+		case 's': //enable camera
+		{
+			if (_instance) {
+				SetRenderWindow(_instance, MEMBER_ID, (void*)_mainWindow, true);
+
+				ModifyVideoStatus(_instance, true);
+			}
+			break;
+		}
+		case 'z'://mute audio
+		{
+			if (_instance) {
+				ModifyAudioStatus(_instance, false);
+			}
+			break;
+		}
+		case 'x'://resume audio
+		{
+			if (_instance) {
+				ModifyAudioStatus(_instance, true);
+			}
+			break;
+		}
+		case 'd':
+		{
+			if (_instance) {
+				EnableMemberVideo(_instance, _member_win_map.begin()->first.c_str(), NULL);
+			}
+
+			break;
+		}
+		case 'f':
+		{
+			if (_instance) {
+				DisableMemberVideo(_instance, _member_win_map.begin()->first.c_str(), NULL);
+			}
+
+			break;
+		}
+		case 'c':
+		{
+			if (_instance) {
+				EnableMemberAudio(_instance, _member_win_map.begin()->first.c_str(), NULL);
+			}
+
+			break;
+		}
+		case 'v':
+		{
+			if (_instance) {
+				DisableMemberAudio(_instance, _member_win_map.begin()->first.c_str(), NULL);
+			}
+
+			break;
+		}
+		default:
+			break;
+		}
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_NEW_REMOTEWINDOWS:
+	{
+		HWND remoteWindow = NULL;
+
+		const char*  pDisplayName = (const char*)lParam;
+
+		if (pDisplayName != NULL) {
+			VVCreateWindow(remoteWindow, 0, WIDTH, HEIGHT, pDisplayName);
+		}
+
+		const char*  pMemberId = (const char*)wParam;
+		if (pMemberId != NULL > 0 && _instance) {
+			char szMemberId[255];
+			strcpy(szMemberId, pMemberId);
+			SetRenderWindow(_instance, szMemberId, (void*)remoteWindow, true);
+		}
+
+		_member_win_map.insert(MemberWindowPair(pMemberId, remoteWindow));
+
+		break;
+	}
+	case WM_CLOSE_WINDOW:
+	{
+		HWND  hWnd = (HWND)lParam;
+		CloseWindow(hWnd);
+		// release .....
+
+		const char* pMemberId = (const char*)wParam;
+
+		MemberWindowMap::iterator it = _member_win_map.find(pMemberId);
+		if (it != _member_win_map.end()) {
+			_member_win_map.erase(it);
+		}
+
+
+		break;
+	}
+	case WM_CLOSE:
+	{
+		CloseWindow(hWnd);
+		// release .....
+
+		break;
+	}
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+int VVCreateWindow(HWND &hwndMain, int winNum, int width, int height, const char* pTitle)
+{
+	static HINSTANCE hinst = NULL;
+	if (hinst == NULL) {
+
+		hinst = GetModuleHandle(0);
+		WNDCLASSEX wcx;
+		wcx.hInstance = hinst;
+		wcx.lpszClassName = TEXT("VideoRenderTest");
+		wcx.lpfnWndProc = (WNDPROC)VVCreateWindowProc;
+		wcx.style = CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+		wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+		wcx.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+		wcx.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wcx.lpszMenuName = NULL;
+		wcx.cbSize = sizeof(WNDCLASSEX);
+		wcx.cbClsExtra = 0;
+		wcx.cbWndExtra = 0;
+		wcx.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
+
+		// Register our window class with the operating system.
+		// If there is an error, exit program.
+		if (!RegisterClassEx(&wcx))
+		{
+			MessageBox(0, TEXT("Failed to register window class!"), TEXT("Error!"), MB_OK | MB_ICONERROR);
+			return 0;
+		}
+	}
+
+	// Create the main window.
+	hwndMain = CreateWindowEx(
+		0, // no extended styles
+		TEXT("VideoRenderTest"), // class name
+		pTitle, // window name
+		WS_OVERLAPPEDWINDOW | WS_MAXIMIZE, // overlapped window
+		800, // horizontal position
+		0, // vertical position
+		width, // width
+		height, // height
+		(HWND)NULL, // no parent or owner window
+		(HMENU)NULL, // class menu used
+		hinst, // instance handle
+		NULL); // no window creation data
+
+	if (!hwndMain)
+		return -1;
+
+	ShowWindow(hwndMain, SW_SHOWDEFAULT);
+	UpdateWindow(hwndMain);
+	return 0;
+}
+
+void OnMemberEnableVideo(const char* pMemberId, const char* pDisplayName)  {
+	printf("OnMemberEnableVideo pMemberId = %s, pDisplayName = %s\n", pMemberId, pDisplayName);
+	if (_instance) {
+		::SendMessage(_mainWindow, WM_NEW_REMOTEWINDOWS, (WPARAM)pMemberId, (LPARAM)(pDisplayName));
+	}
+}
+
+void OnMemberDisableVideo(const char* pMemberId, const char* pDisplayName, void* pRenderWin)  {
+	printf("OnMemberDisableVideo pMemberId = %s, pDisplayName = %s\n", pMemberId, pDisplayName);
+	::SendMessage(_mainWindow, WM_CLOSE_WINDOW, (WPARAM)pMemberId, (LPARAM)(pRenderWin));
+	//ShowWindow((HWND)pRenderWin, SW_HIDE);
+}
+
+int main() {
+	std::cout << "Please input RoomId:" << std::endl;
+
+	std::cin >> _roomId;
+
+	VVCreateWindow(_mainWindow, 0, WIDTH, HEIGHT, TEXT("Local Camera"));
+
+	MSG msg;
+	BOOL bRet;
+	while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
